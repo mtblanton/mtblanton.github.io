6c8c41b169de8c60942454fc20d920e3dc6608cc
Taylor Blanton <matthewtblanton@gmail.com>
2017-08-29 20:06:19 -0500

    I'm dumb, I made keeping the hamburger button on the screen harder than it should've been. Corrected that.

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,7 +5,17 @@ const slideout = new Slideout({
     'tolerance': 70
 });
 
-document.querySelector('.menu__buttons').addEventListener('click', function () {
+const menuButtons = document.querySelector('.menu__buttons');
+
+slideout.on('beforeopen', function () {
+    menuButtons.classList.add('menu__buttons-opened');
+});
+
+slideout.on('beforeclose', function () {
+    menuButtons.classList.remove('menu__buttons-opened');
+});
+
+menuButtons.addEventListener('click', function () {
     this.classList.toggle('menu__buttons-opened');
     slideout.toggle();
 });
